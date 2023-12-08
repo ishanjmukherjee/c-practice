198db625431c7a2f673529e3e392172a2e980a88
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-12-08 16:01:26 +0530

    Update fahr-cels-table-for-loop.c
    added book section; used symbolic constants in loop range

--- fahr-cels-table-for-loop.c
+++ fahr-cels-table-for-loop.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-/* Print Fahrenheit-Celsius table */
+/* CPL Sections 1.3, 1.4
+Print Fahrenheit-Celsius table */
+
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
 
 int main()
 {   
     printf("FAHRENHEIT\tCELSIUS\n");
-    for (int fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     {
         printf("%3d\t\t%5.1f\n", fahr, (fahr-32)*5.0/9.0);
     }
