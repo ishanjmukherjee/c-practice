dca6d0f79709e305d7f64113864ba83ebba6be1c
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-12-08 15:24:58 +0530

    Create celsius-fahrenheit-table.c

---
+++ celsius-fahrenheit-table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* Print Celsius-Fahrenheit table for cels = 0, 20, 40, ..., 300*/
+
+int main()
+{
+    int fahr, cels;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    cels = lower;
+
+    while (cels <= upper)
+    {
+        fahr = (cels * 9/5) + 32;
+        printf("%d\t%d\n", cels, fahr);
+        cels = cels + step;
+    }
+    
+}
